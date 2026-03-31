4d3360971163786c33a11ebc3941bd55a07aee79
aliusman3 <48241839+aliusman3@users.noreply.github.com>
2026-03-31 16:39:47 +0500

    Added error checking

--- maps/main_test.go
+++ maps/main_test.go
@@ -3,12 +3,19 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		assertError(t, err, ErrNotFound)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -16,5 +23,16 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("expected to get an error")
+	}
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
