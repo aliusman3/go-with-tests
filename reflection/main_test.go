5b093d3cf2429305afaf3710ae7afa928c8cbfb7
aliusman3 <48241839+aliusman3@users.noreply.github.com>
2026-04-02 18:09:38 +0500

    Added Reflection section

---
+++ reflection/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			var got []string
+			walk(tt.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, tt.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, tt.ExpectedCalls)
+			}
+		})
+	}
+}
