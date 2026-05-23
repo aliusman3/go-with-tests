36951594198eb95331161b5c47ac760096b4cf36
aliusman3 <48241839+aliusman3@users.noreply.github.com>
2026-05-23 18:46:02 +0500

    Added In Memory Player Store and integration test

---
+++ final-project/in_memory_player_store.go
@@ -0,0 +1,28 @@
+package main
+
+import "sync"
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{
+		map[string]int{},
+		&sync.Mutex{},
+	}
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+	mu    *sync.Mutex
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	score := i.store[name]
+	return score
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	i.store[name]++
+	i.mu.Unlock()
+}
