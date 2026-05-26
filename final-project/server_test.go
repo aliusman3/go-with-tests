ab8136633afb69c23584c6c92b155aa2d1941277
aliusman3 <48241839+aliusman3@users.noreply.github.com>
2026-05-26 12:51:15 +0500

    feat: created file system store

--- final-project/server_test.go
+++ final-project/server_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -126,7 +125,7 @@ func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want s
 
 func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	t.Helper()
-	err := json.NewDecoder(body).Decode(&league)
+	league, err := NewLeague(body)
 
 	if err != nil {
 		t.Fatalf("Unable to parse response from server %q into slice of Player, '%v'", body, err)
